8a4976ff7516028821ad10fa95550803e292b7d8
Juerg Hunziker <juerg.hunziker@gmail.com>
2014-07-20 16:19:02 +0200

    adjusting bird move animation by getting offset of elements

--- Resources/JavaScripts/scripts.js
+++ Resources/JavaScripts/scripts.js
@@ -1,14 +1,21 @@
-// initialize tooltip plugin
-$('.social-buttons a').tooltip();
+$(document).ready(function() {
+    // initialize tooltip plugin
+    $('.social-buttons a').tooltip();
 
-// Returns a reference to the specified CSS rule(s).
+    // wait 2sec to complete rendering of page before initializing bird move animation
+    setTimeout(initBirdMoveAnimation, 2000);
+});
+
+// Returns css rule by name
 function getRule(ruleName) {
-    var rule;
-    var ss = document.styleSheets;
-    for (var i = 0; i < ss.length; ++i) {
+    var rule,
+        ss = document.styleSheets,
+        i, x;
+
+    for (i = 0; i < ss.length; ++i) {
         if(ss[i].cssRules) {
-            // loop through all the rules!
-            for (var x = 0; x < ss[i].cssRules.length; ++x) {
+            // loop through all the rules
+            for (x = 0; x < ss[i].cssRules.length; ++x) {
                 rule = ss[i].cssRules[x];
                 if (rule.type === CSSRule.KEYFRAMES_RULE && rule.name === ruleName) {
                     return rule;
@@ -17,38 +24,40 @@ function getRule(ruleName) {
         }
     }
 }
-var birdMoveRule = getRule("bird-move");
 
-if(birdMoveRule) {
-    var targetPositionOffsetX = 5;
-    var targetPositionOffsetY = 10;
-    console.log("target top: " + $('.target-position').offset().top);
-    console.log("twitter top: " + $('.social-buttons a.twitter').offset().top);
-    var distanceX = $('.target-position').offset().left - $('.social-buttons a.twitter').offset().left - targetPositionOffsetX;
-    var distanceY = $('.target-position').offset().top - $('.social-buttons a.twitter').offset().top - targetPositionOffsetY;
-    console.log(distanceX + " " + distanceY);
-    //birdMoveRule.deleteRule("100%");
-    //birdMoveRule.insertRule("100% { -webkit-transform: scaleX(-1); top: " + distanceY + "px; left: " + distanceX + "px; }");
-    console.log(birdMoveRule);
-}
+function initBirdMoveAnimation() {
+    var birdMoveRule = getRule("bird-move"),
+        targetPositionOffsetX, targetPositionOffsetY, distanceX, distanceY,
+        idleActive = false,
+        idleTime = 10000,
+        startAnimation,
+        timeoutTimer;
 
-var idleActive = false;
-var timeout = function() {
-    idleActive = true;
-    $('.twitter-bird-box').show();
-    $('.twitter-bird-box').addClass('twitter-bird-move');
-    $('.fa-twitter').hide();
-};
-var timeoutTime = 5000;
-var timeoutTimer = setTimeout(timeout, timeoutTime);
-$(document).ready(function() {
+    if(birdMoveRule) {
+        // ajust bird move animation by getting offset of web elements
+        targetPositionOffsetX = 1;
+        targetPositionOffsetY = 13;
+        distanceX = $('.target-position').offset().left - $('.social-buttons a.twitter').offset().left - targetPositionOffsetX;
+        distanceY = $('.target-position').offset().top - $('.social-buttons a.twitter').offset().top - targetPositionOffsetY;
+        birdMoveRule.deleteRule("100%");
+        birdMoveRule.insertRule("100% { -webkit-transform: scaleX(-1); top: " + distanceY + "px; left: " + distanceX + "px; }");
+    }
+
+    startAnimation = function() {
+        idleActive = true;
+        $('.twitter-bird-box').show();
+        $('.twitter-bird-box').addClass('twitter-bird-move');
+        $('.fa-twitter').hide();
+    };
+    timeoutTimer = setTimeout(startAnimation, idleTime);
     $('body').bind('mousemove mousedown keydown', function(event) {
         if(idleActive) {
+            // restore website / stop animation
             $('.fa-twitter').show();
             $('.twitter-bird-box').hide();
             $('.twitter-bird-box').removeClass('twitter-bird-move');
         }
         clearTimeout(timeoutTimer);
-        timeoutTimer = setTimeout(timeout, timeoutTime);
+        timeoutTimer = setTimeout(startAnimation, idleTime);
     });
-});
+}
