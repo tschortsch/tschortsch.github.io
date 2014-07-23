7b171ea14d8b83f7b85896686347e28033e058dc
Juerg Hunziker <juerg.hunziker@gmail.com>
2014-07-23 20:08:28 +0200

    added Grunt configuration

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            page: ['Resources/**/*.js', 'Grunfile.js'],
+            options: {
+                jquery: true,
+                globals: {
+                }
+            }
+        }
+    });
+
+    // Load the plugins
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint']);
+};
