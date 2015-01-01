69be8d1f6faed2ef0e3ac4f3f053de464e594e67
Juerg Hunziker <juerg.hunziker@gmail.com>
2015-01-01 17:06:32 +0100

    fixed async problems in gulp file by returning streams in tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,17 @@ var scsslint = require('gulp-scss-lint');
 var bootlint = require('gulp-bootlint');
 
 gulp.task('lint', function() {
-    gulp.src('/Resources/JavaScripts/*.js')
+    return gulp.src('/Resources/JavaScripts/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(jshint.reporter('fail'));
 });
 gulp.task('scss-lint', function() {
-    gulp.src('/Resrouces/Styles/*.scss')
+    return gulp.src('/Resrouces/Styles/*.scss')
         .pipe(scsslint());
 });
 gulp.task('bootlint', function() {
-    gulp.src(['index.html', '404.html'])
+    return gulp.src(['index.html', '404.html'])
         .pipe(bootlint());
 });
 
