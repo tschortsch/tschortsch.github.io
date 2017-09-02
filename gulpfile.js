75a624efa9e176792dbab3840940b9ae6af3f33d
Juerg Hunziker <juerg.hunziker@gmail.com>
2017-09-02 14:06:50 +0200

    create custom fontawesome pack

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var autoprefixer = require('gulp-autoprefixer');
 var jshint = require('gulp-jshint');
 var sassLint = require('gulp-sass-lint');
 var bootlint = require('gulp-bootlint');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function() {
     return gulp.src('./assets/scss/styles.scss')
@@ -15,6 +17,23 @@ gulp.task('sass', function() {
         .pipe(autoprefixer())
         .pipe(gulp.dest('./assets/css'));
 });
+
+gulp.task('scripts-website', function() {
+    return gulp.src('./assets/js/scripts.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./assets/js'));
+});
+
+gulp.task('scripts-fontawesome', function() {
+    return gulp.src('./assets/js/fontawesome/packs/custom.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./assets/js/fontawesome/packs'));
+});
+
+gulp.task('scripts', ['scripts-website', 'scripts-fontawesome']);
+
 gulp.task('lint', function() {
     return gulp.src('./assets/js/*.js')
         .pipe(jshint())
